cdba8274dc6352ab74c7fb89ff98e89798784ab1
Ilan <ilan.mamontov@gmail.com>
2018-01-07 16:31:57 -0500

    Add basic animations for swiping left/write

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 
 class Deck extends Component {
 
@@ -13,26 +16,77 @@ class Deck extends Component {
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: gesture.dy })
       },
-      onPanResponderRelease: () => {}
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          this.forceSwipe('right');
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe('left');
+        } else { 
+          this.resetPosition();
+        }
+      }
     });
 
     this.state = { panResponder, position };
   }
 
+  forceSwipe(direction) {
+    const distance = direction === 'left' ?  -(SCREEN_WIDTH + 100) : SCREEN_WIDTH + 100
+
+    Animated.timing(this.state.position, {
+      toValue: { x: distance + 50, y: 0},
+      duration: 250
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight } = this.props;
+
+    direction === 'right' ? onSwipeRight() : onSwipeLeft(); 
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0}
+    }).start();
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
+      outputRange: ['-45deg', '0deg', '45deg']
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.state.panResponder.panHandlers} 
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
+
       return this.props.renderCard(item);
     });
   }
 
   render() {
     return (
-      <Animated.View 
-        style={this.state.position.getLayout()}
-        {...this.state.panResponder.panHandlers} 
-      >
+      <View>
         {this.renderCards()}
-      </Animated.View>
+      </View>
     );
   }
 }
