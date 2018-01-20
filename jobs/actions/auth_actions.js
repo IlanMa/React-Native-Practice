5d314ab13e1321ce4426f39d3058426e3d651c9f
Ilan <ilan.mamontov@gmail.com>
2018-01-19 22:47:28 -0500

    Begin using Indeed API

--- jobs/actions/auth_actions.js
+++ jobs/actions/auth_actions.js
@@ -25,5 +25,5 @@ const doFacebookLogin = async (dispatch) => {
   }
 
   await AsyncStorage.setItem('fb_token', token);
-  dispatch({ type:FACEBOOK_LOGIN_SUCCESS, payload: token });
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token });
 }
--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -1,20 +1,62 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
+import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import { Button, Icon } from 'react-native-elements';
+
 
 class MapScreen extends Component {
+
+  state = {
+    mapLoaded: false,
+    region: {
+      longitude: -122,
+      latitude: 37,
+      longitudeDelta: 0.04,
+      latitudeDelta: 0.09
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ mapLoaded: true });
+    }, 500)
+  }
+
+  onRegionChangeComplete = (region) => {
+    this.setState({ region })
+  }
+
   render() {
+    if (!this.state.mapLoaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+
     return (
-      <View>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
-        <Text>MapScreen</Text>
+      <View style={{ flex: 1 }}>
+        <MapView
+          initialRegion={this.state.region}
+          style={{ flex: 1 }}
+          onRegionChangeComplete={this.onRegionChangeComplete}
+        />
+        <View>
+          <Button
+            large
+            title="Search This Area"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+          />
+        </View>
       </View>
     );
   }
 }
+
+
 
 export default MapScreen;
